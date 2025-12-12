346d48013ad1e1bb41b32a1072bf0b3a793d769c
Visali Harikumar <visaliharioffc@gmail.com>
2025-12-12 13:10:48 +0530

    create compound interest calculator

--- exercise1.py
+++ exercise1.py
@@ -1,19 +1,26 @@
-# Practice - Format specifiers
-x = 3343.454555
-y = 93434.232323
-z = -23823.4
+# # Practice - Format specifiers
+# x = 3343.454555
+# y = 93434.232323
+# z = -23823.4
+#
+# # Round to 2 decimal
+# print(f"x value is {x:.2f}")
+# print(f"y value is {y:.2f}")
+# print(f"z value is {z:.2f}")
+#
+# # Using a thousand separator
+# print(f"x value is {x:,}")
+# print(f"y value is {y:,}")
+# print(f"z value is {z:,}")
+#
+# # Combination of specifiers
+# print(f"x value is {x:+,.2f}")
+# print(f"y value is {y:+,.2f}")
+# print(f"z value is {z:+,.2f}")
 
-# Round to 2 decimal
-print(f"x value is {x:.2f}")
-print(f"y value is {y:.2f}")
-print(f"z value is {z:.2f}")
 
-# Using a thousand separator
-print(f"x value is {x:,}")
-print(f"y value is {y:,}")
-print(f"z value is {z:,}")
-
-# Combination of specifiers
-print(f"x value is {x:+,.2f}")
-print(f"y value is {y:+,.2f}")
-print(f"z value is {z:+,.2f}")
+name = input("Enter your name: ")
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name: ")
+print(f"Your name is {name}")
